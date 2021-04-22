4221e4275fde129df624491f77a951bde9de227c
pparkddo <ehdud3453@gmail.com>
2021-04-22 16:07:05 +0900

    Feat: AnnouncementCertificate orphanRemove = true 적용

--- src/main/java/com/gongdb/admin/announcement/entity/Announcement.java
+++ src/main/java/com/gongdb/admin/announcement/entity/Announcement.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -36,7 +37,7 @@ public class Announcement {
     @JoinColumn
     private Position position;
 
-    @OneToMany(mappedBy = "announcement")
+    @OneToMany(mappedBy = "announcement", cascade = CascadeType.PERSIST, orphanRemoval = true)
     private List<AnnouncementCertificate> announcementCertificates = new ArrayList<>();
 
     @NotNull
--- src/test/java/com/gongdb/admin/announcement/RepositoryTest.java
+++ src/test/java/com/gongdb/admin/announcement/RepositoryTest.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.gongdb.admin.announcement.entity.Announcement;
-import com.gongdb.admin.announcement.entity.AnnouncementCertificate;
 import com.gongdb.admin.announcement.entity.Certificate;
 import com.gongdb.admin.announcement.entity.Company;
 import com.gongdb.admin.announcement.entity.Position;
@@ -81,20 +80,59 @@ class RepositoryTests {
         for (Certificate each : certificates) {
             em.persistAndFlush(each);
         }
-        em.persist(announcement);
-        for (AnnouncementCertificate each : announcement.getAnnouncementCertificates()) {
-            em.persistAndFlush(each);
-        }
+        em.persistAndFlush(announcement);
         em.clear();
 
         Announcement a = em.find(Announcement.class, announcement.getId());
 
         assertEquals(company.getId(), a.getCompany().getId());
         assertEquals(position.getId(), a.getPosition().getId());
+        assertEquals(a.getAnnouncementCertificates().size(), certificates.size());
         assertEquals(
             certificates.stream().map(each -> each.getId()).collect(Collectors.toList()),
             a.getAnnouncementCertificates().stream().map(each -> each.getCertificate().getId()).collect(Collectors.toList())
         );
     }
 
+	@Test
+    public void orphanRemoveAnnouncementCertificate() {
+        Company company = Company.builder().name("Test company").build();
+        Position position = Position.builder().name("Test position").build();
+        List<Certificate> certificates = List.of(
+            Certificate.builder().name("certificateA").build(),
+            Certificate.builder().name("certificateB").build()
+        );
+        Announcement announcement = Announcement.builder()
+                                        .company(company)
+                                        .position(position)
+                                        .certificates(certificates)
+                                        .recruitType("recruitType")
+                                        .recruitLevel("recruitLevel")
+                                        .workingType("workingType")
+                                        .receiptTimestamp(LocalDate.of(2021, 4, 22))
+                                        .sequence("sequence")
+                                        .link("link")
+                                        .languageScore(0)
+                                        .languagePerfectScore(0)
+                                        .rank("rank")
+                                        .isEither(true)
+                                        .build();
+        em.persistAndFlush(company);
+        em.persistAndFlush(position);
+        for (Certificate each : certificates) {
+            em.persistAndFlush(each);
+        }
+        em.persistAndFlush(announcement);
+
+        // announcement 를 삭제함으로써 참조를 잃어버린 AnnouncementCertificate 도 같이 삭제한다 (orphanRemove)
+        em.remove(announcement);
+        em.flush();
+        em.clear();
+
+        Long count = em
+                        .getEntityManager()
+                        .createQuery("SELECT COUNT(*) FROM AnnouncementCertificate a", Long.class)
+                        .getSingleResult();
+        assertEquals(0, count);
+    }
 }
